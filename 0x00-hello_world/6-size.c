b78dc6813a813f55434838f43f6ec8e5afff9d39
Ray <yorayasumam@gmmail.com>
2022-09-13 12:54:27 -0700

    For six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,14 @@
 
 /**
  *
- * * main - Prints the sizeof many var types
+ * main - Prints the sizeof many var types
  *
- * *
- *
- * * Return: Always 0 (Success)
+ * 
+ * Description: using the main function 
+ * Return: Always 0 (Success)
  *
  */
-
 int main(void)
-
 {
 
 	char a;
